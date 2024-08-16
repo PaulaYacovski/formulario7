538e7bc52d5c73bb039a5a628ec962a96d03dfaf
Alejandra <alegn.0594@gmail.com>
2024-08-16 12:17:55 -0300

    Cambios JS

--- js/validacion.js
+++ js/validacion.js
@@ -1,35 +1,38 @@
 
 
      
-    function validar() {
-        const nombre = document.getElementById('nombre').value;
-        const apellido = document.getElementById('apellido').value;
-        const email = document.getElementById('email').value;
-        const password1 = document.getElementById('password1').value;
-        const password2 = document.getElementById('password2').value;
+    
+document.getElementById("regBtn").addEventListener("click", function(){  const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+    
+    const terminos = document.getElementById('form-check-label').checked;
+    
+     
+     if (!nombre || !apellido || !email || !password1 || !password2) {
+        showAlertError('Todos los campos deben estar completos.');
         
-        const terminos = document.getElementById('form-check-label').checked;
+    }
+    
+    if (password1.length < 6) {
+        showAlertError('La contraseña debe tener al menos 6 caracteres');
         
-         
-         if (!nombre || !apellido || !email || !password1 || !password2) {
-            showAlertError('Todos los campos deben estar completos.');
-            return;
-        }
-        
-        if (password1.length < 6) {
-            showAlertError('La contraseña debe tener al menos 6 caracteres');
-            return;
-        }
-        
-        if (password1 !== password2) {
-            showAlertError('Las contraseñas no coinciden');
-            return;
-        }
-        
-        showAlertSuccess();
-         
-         };
-document.getElementById("regBtn").addEventListener("click", validarDatos); 
+    }
+    
+    if (password1 !== password2) {
+        showAlertError('Las contraseñas no coinciden');
+    
+    }
+    
+    showAlertSuccess();
+     
+if (!terminos){   
+    showAlertError('Debe aceptar los términos y condiciones.');
+}
+
+}); 
 
 
 function showAlertSuccess() {
